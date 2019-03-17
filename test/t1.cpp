e032ca7c7154beacc129375b21adacd5fb665390
fsl <fsl6559@163.com>
2019-03-17 22:57:18 +0800

    something

--- test/t1.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main(){
-    std::cout << "Enter two number:" << std::endl;
-    int a=0,b=0;
-    std::cin>> a >> b;
-    std::cout<<"The sum of"<<a<<" and "<<b<<" is "<<a+b<<std::endl;
-    return 0;
-}
